90c48081c57152c81336d57bdc42370beffa46cd
Jan Neumann <jan.alexander.neumann@gmail.com>
2023-05-12 09:14:02 +0200

    add dark/light radio button toggle

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // Initialize modules
 const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
@@ -9,7 +9,7 @@ const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
 // Use dart-sass for @use
-sass.compiler = require('dart-sass');
+//sass.compiler = require('dart-sass');
 
 // Sass Task
 function scssTask() {
@@ -42,3 +42,22 @@ function browserSyncServe(cb) {
   });
   cb();
 }
+
+function browserSyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch('*.html', browserSyncReload);
+    watch(
+      ['app/scss/**/*.scss', 'app/**/*.js'],
+      series(scssTask, jsTask, browserSyncReload)
+    );
+}
+  
+// Default Gulp Task
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+
+
